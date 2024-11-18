7b90ee9edb4c5060def1ce18dc18596ef855e37f
donam26 <abcnam26@gmail.com>
2024-11-19 01:46:21 +0700

    update

--- car_maintenance/settings.py
+++ car_maintenance/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'car_maintenance.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
